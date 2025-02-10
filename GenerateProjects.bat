642e4d143866404b61edc0120d58bc75d606976d
Hsuan-Kai (Aaron) Liao <2384288968@qq.com>
2025-02-10 10:37:58 -0500

    Add imgui.ini formatting

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
-echo Enter the IDE to generate (e.g., clion, xcode4, vs2022, gmake2):
+echo Enter the IDE to generate (e.g. xcode4, vs2022, gmake2):
 set /p IDE_NAME=
 
 .premake\Windows\premake5.exe %IDE_NAME%
